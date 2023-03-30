8773bc89deaf34b82e60ae87e127100f53c33cbe
unknown <bayuxbca@gmail.com>
2023-03-30 11:51:45 +0700

    finish ChatMe Realtime Chat Apps

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('dashboard');
+// });
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
